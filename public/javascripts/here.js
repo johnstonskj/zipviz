a1e2e27da96467b496662d2a856dc528ac186ccd
Simon Johnston <simon@johnstonshome.org>
2015-12-08 10:14:48 -0800

    Removed unused content, HERE maps rendering, and default 'users' route.

--- public/javascripts/here.js
+++
@@ -1,4 +0,0 @@
-HERE = {
-    APP_ID: 'YCazFeyLldTCUwU3OfPO', //'hWr37VsFP34FqRnRvzrl',
-    APP_CODE: 'VCPNCEwKf1dA5AtzKCL9gQ' //'VuV5-_ke9QqoPUknw2kWiw'
-}
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const TITLE = 'Zip Vizualizer';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-        res.render('index', { title: 'Zip Viz', heading: 'Zip Group Viewer' });
-});
-
-router.get('/render', function(req, res, next) {
-        res.render('render', { title: 'Zip Viz', heading: 'Zip Group Viewer' });
+        res.render('index', { title: TITLE });
 });
 
 router.get('/lrender', function(req, res, next) {
-        res.render('llrender', { title: 'Zip Viz', heading: 'Zip Group Viewer' });
+        res.render('llrender', { title: TITLE });
+});
+
+router.get('/help', function(req, res, next) {
+        res.render('help', { title: TITLE });
 });
 
 module.exports = router;
